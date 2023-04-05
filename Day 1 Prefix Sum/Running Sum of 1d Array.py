d6f8a3d5b472ea20c6c0df7f87ef6fbd0eb8b719
ARAnsar <aransari18312@gmail.com>
2023-04-05 05:20:07 +0330

    change runnig sum

--- Day 1 Prefix Sum/Running Sum of 1d Array.py
+++ Day 1 Prefix Sum/Running Sum of 1d Array.py
@@ -1,7 +1,6 @@
 class Solution(object):
     def runningSum(self, nums):
-        result= [0] * len(nums)
-        for i in range(len(nums)):
-            for j in range(0,i+1):
-                result[i] = result[i] + nums[j]
-        return result
+        for i in range(1,len(nums)):
+            nums[i] += nums[i-1]
+        
+        return nums
